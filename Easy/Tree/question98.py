ea9b7898dd390bf204f39011adeb8c614a094116
Devin Klepp <33706676+DevinKlepp@users.noreply.github.com>
2019-11-20 16:10:47 -0500

    98. Validate Binary Search Tree
    Using recursion, validate binary search tree

---
+++ Easy/Tree/question98.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isValidBST(self, root):
+        return self.check(root, float("-inf"), float("inf"))
+    
+    def check(self, root, left, right):
+        if not root:
+            return True
+        if not left < root.val < right:
+            return False
+        
+        return self.check(root.left, left, root.val) and self.check(root.right, root.val, right)
+        
+        
