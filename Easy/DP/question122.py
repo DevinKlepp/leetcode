8dea0aae0a6edc8f644f3b21215730f6a826a7a0
Devin Klepp <33706676+DevinKlepp@users.noreply.github.com>
2020-09-19 17:48:17 -0400

    Update question122.py
    Updated stock q2

--- Easy/DP/question122.py
+++ Easy/DP/question122.py
@@ -5,3 +5,11 @@ class Solution:
             if prices[i+1] > prices[i]:
                 profit += prices[i+1] - prices[i]
         return profit
+    
+class Solution1: # More readable
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                profit += prices[i] - prices[i - 1]
+        return profit
