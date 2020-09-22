678fe521a4eced5e010b9f7933588cb3f1614b6b
DevinKlepp <djklepp@gmail.com>
2020-09-21 23:54:55 -0400

    dp stairs cost poblem

---
+++ Easy/DP/question746.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        p1 = cost[0]
+        p2 = cost[1]
+        for i in range(2, len(cost)):
+            p1, p2 = p2, min(p1, p2) + cost[i]
+        return min(p1, p2)
